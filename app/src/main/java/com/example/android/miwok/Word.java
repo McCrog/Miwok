2510736db861cf77a93061736280f4d22e64e270
Alexander <pit_mib@bk.ru>
2017-10-19 20:55:48 +0300

    Modify Word class and WordAdapter for check have Object image ID or not

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -12,8 +12,11 @@ public class Word {
     /** Miwok translation for the word */
     private String mMiwokTranslation;
 
-    // Drawable resource ID
-    private int mImageResourceId;
+    /** Image resource ID for the word */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /** Constant value that represents no image was provided for this word */
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     /**
      * Create a new Word object.
@@ -61,5 +64,12 @@ public class Word {
      */
     public int getImageResourceId() {
         return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
