00fff625f5c1d120aa0f1b540bc335f42aed7597
Alexander <pit_mib@bk.ru>
2017-10-21 16:35:26 +0300

    Modify WordAdapter class, delete test code

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,10 +1,8 @@
 package com.example.android.miwok;
 
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -21,7 +19,7 @@ public class WordAdapter extends ArrayAdapter<Word>  {
 
     /** Resource ID for the background color for this list of words */
     private int mColorResourceId;
-    
+
     /**
      * Create a new {@link WordAdapter} object.
      *
