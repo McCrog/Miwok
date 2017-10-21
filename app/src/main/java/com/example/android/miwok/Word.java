320a7c02fb8a56032b1735dbeb0423f0ad478d1e
Alexander <pit_mib@bk.ru>
2017-10-21 16:32:57 +0300

    Modify Word class and activity, added code for play music

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -18,6 +18,8 @@ public class Word {
     /** Constant value that represents no image was provided for this word */
     private static final int NO_IMAGE_PROVIDED = -1;
 
+    private int mMusicResourseId;
+
     /**
      * Create a new Word object.
      *
@@ -25,9 +27,10 @@ public class Word {
      *                           (such as English)
      * @param miwokTranslation is the word in the Miwok language
      */
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int musicResourseId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mMusicResourseId = musicResourseId;
     }
 
     /**
@@ -39,10 +42,11 @@ public class Word {
      * @param imageResourceId is the drawable resource ID for the image associated with the word
      *
      **/
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int musicResourseId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mMusicResourseId = musicResourseId;
     }
 
     /**
@@ -64,6 +68,13 @@ public class Word {
      */
     public int getImageResourceId() {
         return mImageResourceId;
+    }
+
+    /**
+     * Get the music resource ID.
+     */
+    public int getMusicResourseId() {
+        return mMusicResourseId;
     }
 
     /**
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,8 +1,10 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -19,7 +21,7 @@ public class WordAdapter extends ArrayAdapter<Word>  {
 
     /** Resource ID for the background color for this list of words */
     private int mColorResourceId;
-
+    
     /**
      * Create a new {@link WordAdapter} object.
      *
